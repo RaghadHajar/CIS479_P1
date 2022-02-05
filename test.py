d50f26562f8a63ff770db7d3fd8ee242ca50bd5d
CleverAndWitty <43150101+CleverAndWitty@users.noreply.github.com>
2022-02-04 21:43:53 -0500

    It works now!

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
 import pprint
 import Puzzle as p
+import heapq
+
+def heapsort(iterable):
+        h = []
+        for value in iterable:
+            heapq.heappush(h, value)
+        return [heapq.heappop(h) for i in range(len(h))]
 
 #Step 1 Build Puzzle
 InitialBoard = p.Puzzle.Board()
 OtherBoard = p.Puzzle.Board()
-InitialVector = [8,2,1,5,"-",4,7,3,6]
+InitialVector = ["-",1,2,3,4,5,6,7,8]
+"""
+print("HELLO! \n" +
+    "Welcome to the wonderful puzzle solver! \n +"
+    "This code was made by Raghad and Steven \n +")
+UserInput = input("We would like you to send us a vector of size 9") 
+if UserInput is None:
+    pass
+InitialBoard = UserInput
+"""
 InitialBoard.fnPopulateBoard(InitialVector)
+ExpansionSet = {InitialBoard.ExpansionSetNum : InitialBoard}
 
 #Step 2 Set Goal
 GoalBoard = p.Puzzle.Board()
@@ -14,53 +31,36 @@ GoalBoard.fnPopulateBoard(GoalVector)
 
 #Step 3
 #Calculate current H
-print(InitialBoard.fnCalculateH(GoalBoard))
-print(InitialBoard.fnFindPossibleMoves(GoalBoard))
-#Step 4 
-#Find all possible moves
-#Part a Move to the West cost 2
-#ai Calculate G to make this move
-#aii Calculate what H would be if the board changed so you could make this Move
-#aiii Calculate f (g + h) of this move
-#aiv Add f-value to frontier set position 1
+InitialBoard.fnCalculateH(GoalBoard)
 
-#Part b Move to the North cost 1
-#ai Calculate G to make this move
-#aii Calculate what H would be if the board changed so you could make this Move
-#aiii Calculate f (g + h) of this move
-#aiv Add f-value to frontier set position 2
-
-#Part c Move to the East cost 2
-#ai Calculate G to make this move
-#aii Calculate what H would be if the board changed so you could make this Move
-#aiii Calculate f (g + h) of this move
-#aiv Add f-value to frontier set position 3
-
-#Part d Move to the South cost 3
-#ai Calculate G to make this move
-#aii Calculate what H would be if the board changed so you could make this Move
-#aiii Calculate f (g + h) of this move
-#aiv Add f-value to frontier set position 4
-
-#Step 5 sort frontier set
-#Cost in ascending order
-#Step order # in ascending order 
-
-#Step 6 pop FIFO value from frontier set
-
-#Step 7 use popped value to create new table
-##Increase current G to += self.gFunctionValue from previous state 
-##Recalculate H value store in self.hFunctionValue
-##Increase self.ExpansionSetNum by 1
-
-
-#Step 8 add board as (Board.ExpansionSetNum, Board) in Hashtable (dictionary)
-
-#Go to step 3
-
-#continue until board == goal board
-
-ExpansionSet = {InitialBoard.ExpansionSetNum : InitialBoard,InitialBoard.ExpansionSetNum + 1 : GoalBoard}
-
+while InitialBoard != GoalBoard:
+    #Step 4 
+    #Find all possible moves
+    FrontierSet = InitialBoard.fnFindPossibleMoves(GoalBoard)
+    
+    #Step 5 sort frontier set
+    #Cost in ascending order
+    #Step order # in ascending order 
+    FrontierSet = heapsort(FrontierSet)
+    heapq.heapify(FrontierSet)
+    
+    #Step 6 pop FIFO value from frontier set
+    #Step 7 use popped value to create new table
+    ##Increase current G to += self.gFunctionValue from previous state 
+    ##Recalculate H value store in self.hFunctionValue
+    ##Increase self.ExpansionSetNum by 1
+    BestMove = heapq.heappop(FrontierSet)
+    while BestMove.NewBoard in ExpansionSet.values():
+        BestMove = heapq.heappop(FrontierSet)
+        BestMove.NewBoard.fnCalculateH
+    #Step 8 add board as (Board.ExpansionSetNum, Board) in Hashtable (dictionary)
+    
+    ExpansionSet.update({BestMove.NewBoard.ExpansionSetNum : BestMove.NewBoard})
+    #Go to step 3
+    
+    #continue until board == goal board
+    InitialBoard = BestMove.NewBoard
+    
+    #
 for board in ExpansionSet.values():
     print(board)
