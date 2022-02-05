5283111185ca4e517bb461538da4b49cbc1cd8a5
CleverAndWitty <43150101+CleverAndWitty@users.noreply.github.com>
2022-02-04 19:49:24 -0500

    Update Windy8Puzzle_Najem_Raghad.py
    Completed fnCalculateH and tested
    Modified Find all possible moves (Part4)
    Have to create functioning tuple doubly ordered minpq for frontier set (part 4aiv)

--- Windy8Puzzle_Najem_Raghad.py
+++ Windy8Puzzle_Najem_Raghad.py
@@ -23,6 +23,7 @@ class Puzzle: #Raghad and #Najem
         ,PuzzleGoalIndex = ()
         ,GValue = 0
         ,HValue = 0
+        ,FValue = 0 
         ):
             self.InitialOrder = InitialOrder 
             self.StartingIndex = StartingIndex
@@ -127,6 +128,16 @@ class Puzzle: #Raghad and #Najem
             IndexPosition = self.fnFind("-")
             return IndexPosition
             
+        def fnSwap(self, IndexStart, IndexEnd):
+            BeginRow    =   IndexStart[0]
+            BeginCol    =   IndexStart[1]
+            EndRow      =   IndexEnd[0]
+            EndCol      =   IndexEnd[1]
+            SwapBoard = copy.deepcopy(self)
+            SwapBoard.arrayBoard[EndRow][EndCol] = self.arrayBoard[BeginRow][BeginCol]
+            SwapBoard.arrayBoard[BeginRow][BeginCol] = self.arrayBoard[EndRow][EndCol]
+            return SwapBoard
+            
         def fnFindPossibleMoves(self, tupleIndex=()):
             """ Check for the possible movesets in the directed way:
             West, North, East, South
@@ -145,40 +156,74 @@ class Puzzle: #Raghad and #Najem
             BlankRow = IndexPosStart[0]
             BlankCol = IndexPosStart[1]
             
-            Move1 = Move()
-            Move2 = Move()
-            Move3 = Move()
-            Move4 = Move()
+            Move1 = Move(
+                StartingIndex = (BlankRow, BlankCol)
+                ,InitialOrder = 1)
+            Move2 = Move(
+                StartingIndex = (BlankRow, BlankCol)
+                ,InitialOrder = 2)
+            Move3 = Move(
+                StartingIndex = (BlankRow, BlankCol)
+                ,InitialOrder = 3)
+            Move4 = Move(
+                StartingIndex = (BlankRow, BlankCol)
+                ,InitialOrder = 4)
             
             if BlankCol != 0: #skip west if border
-                Move1 = BlankCol - 1
-                Move1Cost = 2
+                Move1.GValue = self.gFunctionValue + 2
+                Move1.NewIndex = (BlankRow, BlankCol - 1)
+                Move1.PuzzleNum = self.arrayBoard[Move1.NewIndex[0]][Move1.NewIndex[1]]
+                PossibleExpansionSet1 = self.fnSwap(IndexPosStart, Move1.NewIndex)
+                Move1.HValue = PossibleExpansionSet1.fnCalculateH()
+                Move1.FValue = Move1.HValue + Move1.GValue
                 ### Add function call to add to PQFrontier both the move and the cost###
             
             if BlankRow != 0: #skip north if border
-                Move2 = BlankRow - 1 
-                Move2Cost = 1
+                Move2.GValue = self.gFunctionValue + 1
+                Move2.NewIndex = (BlankRow - 1, BlankCol)
+                Move2.PuzzleNum = self.arrayBoard[Move2.NewIndex[0]][Move2.NewIndex[1]]
+                PossibleExpansionSet2 = self.fnSwap(IndexPosStart, Move2.NewIndex)
+                Move2.HValue = PossibleExpansionSet2.fnCalculateH()
+                Move2.FValue = Move2.HValue + Move2.GValue
                 ### Add function call to add to PQFrontier both the move and the cost###
                 
-            if BlankCol != len(self.arrayBoard) - 1: #skip east if border
-                Move3 = BlankCol + 1 
-                Move3Cost = 2
+            if BlankCol != len(self.arrayBoard[0]) - 1: #skip east if border
+                Move3.GValue = self.gFunctionValue + 2
+                Move3.NewIndex = (BlankRow, BlankCol + 1)
+                Move3.PuzzleNum = self.arrayBoard[Move3.NewIndex[0]][Move3.NewIndex[1]]
+                PossibleExpansionSet3 = self.fnSwap(IndexPosStart, Move3.NewIndex)
+                Move3.HValue = PossibleExpansionSet3.fnCalculateH()
+                Move3.FValue = Move3.HValue + Move3.GValue
                 ### Add function call to add to PQFrontier both the move and the cost###
             
             if BlankRow != len(self.arrayBoard) - 1: #skip south if border
-                Move4 = BlankRow + 1 
-                Move4Cost = 4
+                Move4.GValue = self.gFunctionValue + 3
+                Move4.NewIndex = (BlankRow + 1, BlankCol)
+                Move4.PuzzleNum = self.arrayBoard[Move4.NewIndex[0]][Move4.NewIndex[1]]
+                PossibleExpansionSet4 = self.fnSwap(IndexPosStart, Move4.NewIndex)
+                Move4.HValue = PossibleExpansionSet4.fnCalculateH()
+                Move4.FValue = Move4.HValue + Move4.GValue
                 ### Add function call to add to PQFrontier both the move and the cost###
         
-        def fnCalculateH(self, CurrentIndex = (), GoalIndex = (), GoalBoard = None):
+        def fnCalculateH(self, GoalBoard, CurrentIndex = (), GoalIndex = ()):
             if GoalBoard is not None:
-                if CurrentIndex == ():
-                    CurrentIndex = self.fnFindBlank()
-                if GoalIndex == ():
-                    GoalBoard.fnFindBlank()
+                CurrentH = 0
+                if CurrentIndex == () and GoalIndex == ():
+                    for row in range(len(self.arrayBoard)):
+                        for col in range(len(self.arrayBoard[row])):
+                            CurrentPuzNum = self.arrayBoard[row][col]
+                            GoalIndex = GoalBoard.fnFind(CurrentPuzNum)
+                            CurrentH += (math.sqrt((GoalIndex[1] - col )**2 ))*2
+                            NorthSouth = GoalIndex[0] - row
+                            if NorthSouth < 0:
+                                CurrentH += math.sqrt((NorthSouth)**2 )
+                            elif NorthSouth > 0:
+                                CurrentH += (math.sqrt((NorthSouth)**2 ))*3
+                            
             else:
                 print("Hey, I think you need to setup a GoalBoard before we keep going")
-            pass
+            
+            return CurrentH
         
         def fnCalculateG(self, CostOfNewMove, PreviousG = None):
             """Can be used to calculate g(n) """
